7f3a4b20d7676743f882144ad99a755f9b5d311c
Eric <liuxiao1998@sjtu.edu.cn>
2017-01-13 13:51:17 +0800

    You will like it>

--- cpp/aptrmv/aptrmv.cpp
+++ cpp/aptrmv/aptrmv.cpp
@@ -1,49 +1,34 @@
+#include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+using namespace std;
 int main(int argc,char *argv[])
 {
+	string cmd="sudo apt-get purge";
 	int i=1;
-	extern void popenc(char*cm);
 	if(argv[1]==NULL)
 	{
 		printf("错误:请加上要卸载的软件名\n");
 		exit(1);
 	 
 	}
-	char cmd[]="sudo apt-get purge";
-	char sp[]=" ";
-	char *check;
-    check=(char *)malloc(100);
+	
 	
 	while(i<=argc-1)
 	{
-		strcat(cmd,sp);
-		strcat(cmd,argv[i]);
+		cmd.append(" ");
+		cmd.append(argv[i]);
 		i++;
 	}
-	printf("即将执行命令:%s\n",cmd);
-recheck:printf("要继续吗？（y or n）\n");
-	scanf("%s",check);
-	if(strcmp(check,"n")==0)
-	{
-		exit(1);
-	}
-	if(strcmp(check,"y")!=0)
-	{
-		goto recheck;
-	}
-
-	popenc((char*)cmd);
-	popenc((char*)"sudo apt-get autoremove");
-	popenc((char*)"sudo apt-get clean");
-	popenc((char*)"dpkg -l |grep ^rc|awk '{print $2}' |sudo xargs dpkg -P");
 
 
+	system((char *)cmd.data());
+	system((char*)"sudo apt-get autoremove");
+	system((char*)"sudo apt-get clean");
+	system((char*)"dpkg -l |grep ^rc|awk '{print $2}' |sudo xargs dpkg -P");
 
 
-	free(check);
-	check=NULL;
 	return 0;
 }
 
