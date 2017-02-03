4fc2e241cb28f808942bc80475538b8e90dc18e2
liuxiao1998 <liuxiao1998@sjtu.edu.cn>
2017-02-03 17:38:20 +0800

    engilish

---
+++ cpp-initialize/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+#include<fstream>
+using namespace std;
+int main()
+{
+  string ss;
+  cout<<"cpp's name:"<<endl;
+  cin>>ss;
+  ofstream mm(ss.data());
+
+  mm<<"#include<iostream>"<<endl<<"using namespace std;"<<endl<<"int main()"<<endl<<"{"<<endl<<endl<<"}";
+  mm<<endl<<"/*"<<endl<<"一直在找寻着"<<endl<<"在何处有你的身影"<<endl<<"在这星星都像要落下来的夜里"<<endl<<"我无法再欺骗自己"<<endl<<"one more time"<<endl<<"*/";
+  mm.close();
+}
---
+++ cpp-initialize/name.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+}
+/*
+一直在找寻着
+在何处有你的身影
+在这星星都像要落下来的夜里
+我无法再欺骗自己
+one more time
+*/
