6cd7a7fac4a76ed1e7080bcda141675a207c2921
liuxiao/eric <liuxiao1998@sjtu.edu.cn>
2017-02-10 12:34:39 +0800

    AUTO

---
+++ class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+
+class stu{
+public:
+  string name;
+  int id;
+
+};
+
+int main(int argc,char *argv[])
+{
+  stu student[100];
+  cin>>student[0].name;
+  cout<<student[0].name;
+  cin>>student[1].name;
+
+
+}
--- math/main.cpp
+++ math/main.cpp
@@ -4,12 +4,7 @@ using namespace std;
 int i;
 int main()
 {
-  for(i=0;i<=100000;++i)
-    {
-    if(i%2==1&&i%3==0&&i%4==1&&i%5==4&&i%6==3&&i%7==0&&i%8==1&&i%9==0)
-    {
-      cout<<i<<endl;
-    }
-  }
-  getchar();
+  char sig;
+  cout<<(sig=getchar())<<endl;
+  cin>>sig;
 }
